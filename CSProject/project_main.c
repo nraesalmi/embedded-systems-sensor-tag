6bcc78b0ad47896189222e32d49c0fa3925cb8cf
Bui Dai Hieu <91104220+hbui138@users.noreply.github.com>
2024-11-22 15:24:00 +0200

    Update project_main.c

--- CSProject/project_main.c
+++ CSProject/project_main.c
@@ -25,97 +25,14 @@
 #include "sensors/mpu9250.h"
 #include "sensors/buzzer.h"
 
-#define NOTE_B0  31
-#define NOTE_C1  33
-#define NOTE_CS1 35
-#define NOTE_D1  37
-#define NOTE_DS1 39
-#define NOTE_E1  41
-#define NOTE_F1  44
-#define NOTE_FS1 46
-#define NOTE_G1  49
-#define NOTE_GS1 52
-#define NOTE_A1  55
-#define NOTE_AS1 58
-#define NOTE_B1  62
-#define NOTE_C2  65
-#define NOTE_CS2 69
-#define NOTE_D2  73
-#define NOTE_DS2 78
-#define NOTE_E2  82
-#define NOTE_F2  87
-#define NOTE_FS2 93
-#define NOTE_G2  98
-#define NOTE_GS2 104
-#define NOTE_A2  110
-#define NOTE_AS2 117
-#define NOTE_B2  123
-#define NOTE_C3  131
-#define NOTE_CS3 139
-#define NOTE_DB3 139
-#define NOTE_D3  147
-#define NOTE_DS3 156
-#define NOTE_EB3 156
-#define NOTE_E3  165
-#define NOTE_F3  175
-#define NOTE_FS3 185
-#define NOTE_G3  196
-#define NOTE_GS3 208
-#define NOTE_A3  220
-#define NOTE_AS3 233
-#define NOTE_B3  247
-#define NOTE_C4  262
-#define NOTE_CS4 277
-#define NOTE_D4  294
-#define NOTE_DS4 311
-#define NOTE_E4  330
-#define NOTE_F4  349
-#define NOTE_FS4 370
-#define NOTE_G4  392
-#define NOTE_GS4 415
-#define NOTE_A4  440
-#define NOTE_AS4 466
-#define NOTE_B4  494
 #define NOTE_C5  523
-#define NOTE_CS5 554
-#define NOTE_D5  587
-#define NOTE_DS5 622
-#define NOTE_E5  659
-#define NOTE_F5  698
-#define NOTE_FS5 740
-#define NOTE_G5  784
-#define NOTE_GS5 831
-#define NOTE_A5  880
-#define NOTE_AS5 932
-#define NOTE_B5  988
 #define NOTE_C6  1047
-#define NOTE_CS6 1109
-#define NOTE_D6  1175
-#define NOTE_DS6 1245
-#define NOTE_E6  1319
+#define NOTE_F5  698
 #define NOTE_F6  1397
-#define NOTE_FS6 1480
-#define NOTE_G6  1568
-#define NOTE_GS6 1661
-#define NOTE_A6  1760
-#define NOTE_AS6 1865
-#define NOTE_B6  1976
-#define NOTE_C7  2093
-#define NOTE_CS7 2217
-#define NOTE_D7  2349
-#define NOTE_DS7 2489
-#define NOTE_E7  2637
-#define NOTE_F7  2794
-#define NOTE_FS7 2960
-#define NOTE_G7  3136
-#define NOTE_GS7 3322
-#define NOTE_A7  3520
-#define NOTE_AS7 3729
-#define NOTE_B7  3951
-#define NOTE_C8  4186
-#define NOTE_CS8 4435
-#define NOTE_D8  4699
-#define NOTE_DS8 4978
+#define NOTE_G5  784
+#define NOTE_A5  880
+#define NOTE_AS4 466
+#define NOTE_AS5 932
 #define REST     0
 
 #define PI 3.14159265
@@ -138,7 +55,7 @@ double ambientLight = -1000.0;
 
 // Global variables for MPU9250 data
 float ax, ay, az, gx, gy, gz;
-float roll, pitch;
+float roll;
 
 // Turn variable for i2c handling
 char turn = 'j';
@@ -148,6 +65,7 @@ char temp = NULL;
 
 // Boolean for checking button is pressed to send SOS signal
 bool sendSOS = false;
+bool isBuzzing = false;
 char morseList[15];
 const char SOS[15] = {'.', '.', '.', ' ', '-', '-', '-', ' ', '.', '.', '.', ' ', ' '};
 char beepMorse[100];
@@ -193,6 +111,8 @@ static const I2CCC26XX_I2CPinCfg i2cMPUCfg = {
 void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
     sendSOS = true; // Send SOS signal
 }
+
+
 
 /* Task Functions */
 Void uartTaskFxn(UArg arg0, UArg arg1) {
@@ -258,38 +178,23 @@ Void uartTaskFxn(UArg arg0, UArg arg1) {
                 }
                 temp = morseLetter;
             }
-            sprintf(str, "\nRoll: %.2f degrees\nPitch: %.2f degrees\n "
-                    "Gyroscope: gx=%.2f dps, gy=%.2f dps, gz=%.2f dps\n "
-                    "Accelerometer: ax=%.2f m/s^2, ay=%.2f m/s^2, az=%.2f m/s^2\n",
-                    roll, pitch, gx, gy, gz, ax, ay, az);
-            //System_printf("%s\n", str);
             System_flush();
-            //OPTState = WAITING;
             MPUState = WAITING;
         }
 
         // Define a buffer to hold the incoming message
         unsigned char message[100];
-
+        char echo_msg[100];
         // UART read for reading messages
         Task_sleep(500000 / Clock_tickPeriod);
-        unsigned int bytesRead = UART_read(uart, message, sizeof(message) - 1); // Read into the buffer, leaving space for null terminator
-
-        if (bytesRead != 0) {
+        uint8_t bytesRead = UART_read(uart, message, sizeof(message) - 1); // Read into the buffer, leaving space for null terminator
+        sprintf(echo_msg, "%c\r\n\0", message);
+        if (!isBuzzing) {
             // message[bytesRead] = '\0';
             strcpy(beepMorse, message);
             Task_sleep(500000 / Clock_tickPeriod);
-
-//            if(message[0] == '-' || message[0] == '.') {
-//                int i;
-//                char str[10];
-//                for (i = 0; i < strlen(message[i]); i++) {
-//                    sprintf(str, "%c\r\n\0", message[i]);
-//                    UART_write(uart, str, strlen(str)); // Write the received message back
-//                }
-//            }
+            //memset(message, 0, sizeof(message));
         }
-        message[bytesRead] = '\0';
 
         // Twice per second, you can modify this
         Task_sleep(500000 / Clock_tickPeriod);
@@ -328,17 +233,19 @@ Void buzzerFxn(UArg arg0, UArg arg1) {
                 buzzerSetFrequency(melody[note]);
 
                 int pauseBetweenNotes = duration * 1.30;
-                Task_sleep(pauseBetweenNotes*1);
+                Task_sleep(pauseBetweenNotes*10);
               }
             buzzerClose();
         }
         Task_sleep(10000 / Clock_tickPeriod);
 
         if (beepMorse[0] == '.' || beepMorse[0] == '-') {
-            buzzerOpen(hBuzzer);
+            isBuzzing = true;
+
             int i;
             for (i = 0; i < strlen(beepMorse); i++) {
                 System_printf(beepMorse[i]);
+                buzzerOpen(hBuzzer);
 
                 // Handle dash (-)
                 if (beepMorse[i] == '-') {
@@ -360,10 +267,10 @@ Void buzzerFxn(UArg arg0, UArg arg1) {
                 else {
                     break;
                 }
-
+                buzzerClose();
 
             }
-            buzzerClose();
+            isBuzzing = false;
 
             // Clear beepMorse array once all processing is done
             for (i = 0; i < strlen(beepMorse); i++) {
@@ -442,7 +349,6 @@ Void mpuSensorTaskFxn(UArg arg0, UArg arg1) {
 
             mpu9250_get_data(&i2cMPU, &ax, &ay, &az, &gx, &gy, &gz);
             roll = atan2(ay, az) * 180.0 / PI;
-            pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180.0 / PI;
             MPUState = DATA_READY;
 
         }
